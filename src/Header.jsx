31030946b552af63efd11375569bd5b8dc254520
kwaktato <109459319+kwaktato@users.noreply.github.com>
2024-11-07 18:39:52 +0900

    feat: Header 위치 고정

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,6 +7,12 @@ const HeaderContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 2;
 `;
 
 const HeaderWrapper = styled.div`
