f7ccdf3617dedb80b6f15fc851e0c42873d152dc
chaechaen <leece144@gmail.com>
2024-11-15 16:07:22 +0900

    feat: 모집 알림 신청 버튼 클릭 시 맨 위로 스크롤

--- src/pages/recruitment/RecruitmentPage.jsx
+++ src/pages/recruitment/RecruitmentPage.jsx
@@ -4,8 +4,21 @@ import RecruitmentTarget from './RecruitmentTarget';
 import RecruitmentSchedule from './RecruitmentSchedule';
 import Notice from './Notice';
 import FAQ from './FAQ';
+import Challenge from "../../components/common/Challenge";
+import styled from 'styled-components';
 
 const RecruitmentPage = () => {
+
+  // 페이지 상단으로 스크롤하는 함수
+  const scrollToTop = (e) => {
+    // 기존 클릭 이벤트 막기
+    e.stopPropagation();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
   return (
     <div>
       <RecruitmentClosed />
@@ -14,8 +27,18 @@ const RecruitmentPage = () => {
       <RecruitmentSchedule/>
       <Notice/>
       <FAQ/>
+      <ChallengeWrapper onClick={(e) => scrollToTop(e)}>
+        <Challenge />
+      </ChallengeWrapper>
     </div>
   );
 };
+
+const ChallengeWrapper = styled.div`
+  // Challenge 컴포넌트의 버튼을 감싸서 클릭 이벤트를 가로챔
+  button {
+    cursor: pointer;
+  }
+`;
 
 export default RecruitmentPage;
