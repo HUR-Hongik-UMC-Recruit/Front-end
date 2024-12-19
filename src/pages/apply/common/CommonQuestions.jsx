fcd39161db6dd19baa4aba705a01b7217c485fae
chaechaen <leece144@gmail.com>
2024-12-19 16:59:47 +0900

    fix: 질문 구성 수정

--- src/pages/apply/common/CommonQuestions.jsx
+++ src/pages/apply/common/CommonQuestions.jsx
@@ -14,8 +14,7 @@ import {
   InfoText,
   StyledRadioGroup,
   RadioButton,
-  RadioDot,
-  RadioCircle,
+  Grid,
 } from "../../../components/apply/common/SectionStyles";
 
 const CommonQuestions = () => {
@@ -46,50 +45,26 @@ const CommonQuestions = () => {
         </FormGroup>
 
         <FormGroup>
+          <Label>3. UMC에 임하는 각오를 서술해주세요.</Label>
+          <TextArea placeholder="500자 이내로 작성해주세요." />
+        </FormGroup>
+
+        <FormGroup>
+          <Label>4. UMC는 학기 중에 배운 것을 바탕으로 방학 동안 팀을 구성해 앱 런칭을 진행합니다. 실제로 어떤 서비스를 개발하고 싶은지 서술해주세요. </Label>
+          <TextArea placeholder="500자 이내로 작성해주세요." />
+        </FormGroup>
+
+        <FormGroup>
           <Label>
-            3. GitHub를 이용해 프로젝트를 한 경험이 있다면 GitHub 주소를
+            6. GitHub를 이용해 프로젝트를 한 경험이 있다면 GitHub 주소를
             남겨주세요.
           </Label>
           <Input type="text" placeholder="예) http://github.com/example" />
         </FormGroup>
 
         <FormGroup>
-          <Label>4. 스터디 리더를 희망하시나요?</Label>
-          <StyledRadioGroup>
-            <RadioButton
-              $active={selectedOption === "예"}
-              onClick={() => handleRadioClick("예")}
-            >
-              <RadioCircle $active={selectedOption === "예"}>
-                {selectedOption === "예" && <RadioDot />}
-              </RadioCircle>
-              예
-            </RadioButton>
+          <Label>7. 스터디 리더를 희망하시나요?</Label>
 
-            <RadioButton
-              $active={selectedOption === "아니요"}
-              onClick={() => handleRadioClick("아니요")}
-            >
-              <RadioCircle $active={selectedOption === "아니요"}>
-                {selectedOption === "아니요" && <RadioDot />}
-              </RadioCircle>
-              아니요
-            </RadioButton>
-
-            <RadioButton
-              $active={selectedOption === "고민해보겠습니다"}
-              onClick={() => handleRadioClick("고민해보겠습니다")}
-            >
-              <RadioCircle $active={selectedOption === "고민해보겠습니다"}>
-                {selectedOption === "고민해보겠습니다" && <RadioDot />}
-              </RadioCircle>
-              고민해보겠습니다
-            </RadioButton>
-          </StyledRadioGroup>
-        </FormGroup>
-
-        <FormGroup>
-          <Label>5. UMC에 임하는 각오를 서술해주세요.</Label>
           <InfoText>
             스터디 리더란 매주 진행되는 스터디의 리더를 의미합니다. 선수 지식이
             있지 않아도 열심히 공부할 열정과 책임감이 있다면 스터디 리더 지원이
@@ -97,8 +72,31 @@ const CommonQuestions = () => {
             이후에 스터디 리더 지원을 따로 받을 예정이며, 스터디 리더 확정이
             아니니 부담가지지 않으셔도 됩니다.
           </InfoText>
-          <TextArea placeholder="500자 이내로 작성해주세요." />
+
+          <Grid
+            style={{
+              display: "flex",
+              justifyContent: "flex-end"
+            }}
+          >
+            <StyledRadioGroup>
+              <RadioButton
+                $active={selectedOption === "예"}
+                onClick={() => handleRadioClick("예")}
+              >
+                예
+              </RadioButton>
+
+              <RadioButton
+                $active={selectedOption === "아니요"}
+                onClick={() => handleRadioClick("아니요")}
+              >
+                아니요
+              </RadioButton>
+            </StyledRadioGroup>
+          </Grid>
         </FormGroup>
+
 
         <ButtonWrapper>
           <Button>다음</Button>
