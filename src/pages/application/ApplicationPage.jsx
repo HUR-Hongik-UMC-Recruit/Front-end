6857e6e75f0f576aedbb05e06d080d3719910958
kwaktato <109459319+kwaktato@users.noreply.github.com>
2024-12-21 02:38:47 +0900

    feat: 지원서 내용 들어가야 하는 표시

--- src/pages/application/ApplicationPage.jsx
+++ src/pages/application/ApplicationPage.jsx
@@ -1,5 +1,17 @@
+import styled from "styled-components";
+
+const ApplicationContent = styled.div`
+  width: 100%;
+`;
+
 const ApplicationPage = () => {
-  return <div></div>;
+  return (
+    <ApplicationContent>
+      {/* <Title />
+        <Verification />
+        <PersonalInfo /> */}
+    </ApplicationContent>
+  );
 };
 
 export default ApplicationPage;
