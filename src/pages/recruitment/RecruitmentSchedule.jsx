3496dd2af8c6219099bb24f9026e302a623071f0
kwaktato <109459319+kwaktato@users.noreply.github.com>
2025-01-06 00:05:59 +0900

    feat: 데이터 분리 작업

--- src/pages/recruitment/RecruitmentSchedule.jsx
+++ src/pages/recruitment/RecruitmentSchedule.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 import SectionHeader from "../../components/common/SectionHeader";
+import scheduleData from "../../data/recruitment/ScheduleData";
 
 const RecruitmentSchedule = () => {
-  const scheduleData = [
-    { title: "서류 접수", date: "08.26 (월) ~ 09.06 (금)" },
-    { title: "1차 발표", date: "09.08 (일)" },
-    { title: "면접", date: "09.09 (월) ~ 09.11 (수)" },
-    { title: "최종 발표", date: "09.13 (금)" },
-  ];
-
   return (
     <BackgroundContainer>
       <ContentWrapper>
