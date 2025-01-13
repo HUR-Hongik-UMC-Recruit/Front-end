09a6155338fb2b95ba407ec9234d11ce1c970fc5
kwaktato <109459319+kwaktato@users.noreply.github.com>
2025-01-13 16:49:27 +0900

    feat: 제일 위 텍스트 애니메이션 확정

--- src/pages/home/HomeMain.jsx
+++ src/pages/home/HomeMain.jsx
@@ -44,6 +44,7 @@ const blink = keyframes`
 const BTRTitleTyping = styled.h1`
   display: flex;
   align-self: center;
+  justify-content: center;
   margin: 0;
 
   color: #90e6c9;
@@ -55,216 +56,14 @@ const BTRTitleTyping = styled.h1`
   line-height: 125%; /* 6.5625rem */
   letter-spacing: -0.105rem;
 
-  // &::after {
-  //   content: "";
-  //   display: inline-block;
-  //   width: 0.5rem;
-  //   height: 100%;
-  //   background-color: #ffffff60;
-  //   animation: ${blink} 1s infinite;
-  // }
-`;
-
-const reveal = keyframes`
-  0% {
-    opacity:0;
-    transform: translateY(-100%);
+  &::after {
+    content: "";
+    display: inline-block;
+    width: 0.25rem;
+    height: 5.25rem;
+    background-color: #edf4f5;
+    animation: ${blink} 1s infinite;
   }
-  100% {
-    transform: translateY(0);
-  }`;
-
-const BTRTitleReveal = styled.div`
-  display: flex;
-  align-self: center;
-  margin: 0;
-
-  color: #90e6c9;
-  text-align: center;
-  font-family: "Pretendard Variable";
-  font-size: 5.25rem;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 125%; /* 6.5625rem */
-  letter-spacing: -0.105rem;
-
-  animation: ${reveal} 1s cubic-bezier(0.77, 0, 0.175, 1) forwards;
-`;
-
-const flick = keyframes`
-  0% {
-    opacity: 0;
-  }
-  10% {
-    opacity: 0;
-    text-shadow: none;
-  }
-  10.1% {
-    opacity: 1;
-    text-shadow: none;
-  }
-  10.2% {
-    opacity: 0;
-    text-shadow: none;
-  }
-  20% {
-    opacity: 0;
-    text-shadow: none;
-  }
-  20.1% {
-    opacity: 1;
-    text-shadow: 0 0 30px rgba(255, 255, 255, 0.25);
-  }
-  20.6% {
-    opacity: 0;
-    text-shadow: none;
-  }
-  30% {
-    opacity: 0;
-    text-shadow: none;
-  }
-  30.1% {
-    opacity: 1;
-    text-shadow: 0 0 30px rgba(255, 255, 255, 0.45), 0 0 60px rgba(255, 255, 255, 0.25);
-  }
-  30.5% {
-    opacity: 1;
-    text-shadow: 0 0 30px rgba(255, 255, 255, 0.45), 0 0 60px rgba(255, 255, 255, 0.25);
-  }
-  30.6% {
-    opacity: 0;
-    text-shadow: none;
-  }
-  45% {
-    opacity: 0;
-    text-shadow: none;
-  }
-  45.1% {
-    opacity: 1;
-    text-shadow: 0 0 30px rgba(255, 255, 255, 0.45), 0 0 60px rgba(255, 255, 255, 0.25);
-  }
-  50% {
-    opacity: 1;
-    text-shadow: 0 0 30px rgba(255, 255, 255, 0.45), 0 0 60px rgba(255, 255, 255, 0.25);
-  }
-  55% {
-    opacity: 1;
-    text-shadow: 0 0 30px rgba(255, 255, 255, 0.45), 0 0 60px rgba(255, 255, 255, 0.25);
-  }
-  55.1% {
-    opacity: 0;
-    text-shadow: none;
-  }
-  57% {
-    opacity: 0;
-    text-shadow: none;
-  }
-  57.1% {
-    opacity: 1;
-    text-shadow: 0 0 30px rgba(255, 255, 255, 0.55), 0 0 60px rgba(255, 255, 255, 0.35);
-  }
-  60% {
-    opacity: 1;
-    text-shadow: 0 0 30px rgba(255, 255, 255, 0.55), 0 0 60px rgba(255, 255, 255, 0.35);
-  }
-  60.1% {
-    opacity: 0;
-    text-shadow: none;
-  }
-  65% {
-    opacity: 0;
-    text-shadow: none;
-  }
-  65.1% {
-    opacity: 1;
-    text-shadow: 0 0 30px rgba(255, 255, 255, 0.55), 0 0 60px rgba(255, 255, 255, 0.35), 0 0 100px rgba(255, 255, 255, 0.1);
-  }
-  75% {
-    opacity: 1;
-    text-shadow: 0 0 30px rgba(255, 255, 255, 0.55), 0 0 60px rgba(255, 255, 255, 0.35), 0 0 100px rgba(255, 255, 255, 0.1);
-  }
-  75.1% {
-    opacity: 0;
-    text-shadow: none;
-  }
-  77% {
-    opacity: 0;
-    text-shadow: none;
-  }
-  77.1% {
-    opacity: 1;
-    text-shadow: 0 0 30px rgba(255, 255, 255, 0.55), 0 0 60px rgba(255, 255, 255, 0.4), 0 0 110px rgba(255, 255, 255, 0.2), 0 0 100px rgba(255, 255, 255, 0.1);
-  }
-  85% {
-    opacity: 1;
-    text-shadow: 0 0 30px rgba(255, 255, 255, 0.55), 0 0 60px rgba(255, 255, 255, 0.4), 0 0 110px rgba(255, 255, 255, 0.2), 0 0 100px rgba(255, 255, 255, 0.1);
-  }
-  85.1% {
-    opacity: 0;
-    text-shadow: none;
-  }
-  86% {
-    opacity: 0;
-    text-shadow: none;
-  }
-  86.1% {
-    opacity: 1;
-    text-shadow: 0 0 30px rgba(255, 255, 255, 0.6), 0 0 60px rgba(255, 255, 255, 0.45), 0 0 110px rgba(255, 255, 255, 0.25), 0 0 100px rgba(255, 255, 255, 0.1);
-  }
-  100% {
-    opacity: 1;
-    text-shadow: 0 0 30px rgba(255, 255, 255, 0.6), 0 0 60px rgba(255, 255, 255, 0.45), 0 0 110px rgba(255, 255, 255, 0.25), 0 0 100px rgba(255, 255, 255, 0.1);
-  }
-}
-`;
-
-const BTRTitleFlick = styled.div`
-  display: flex;
-  align-self: center;
-  margin: 0;
-
-  color: #90e6c9;
-  text-align: center;
-  font-family: "Pretendard Variable";
-  font-size: 5.25rem;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 125%; /* 6.5625rem */
-  letter-spacing: -0.105rem;
-
-  animation: ${flick} 4s linear both;
-`;
-
-const tracking = keyframes`
-  0% {
-    letter-spacing: -0.5em;
-    opacity: 0;
-  }
-  40% {
-    opacity: 0.6;
-  }
-  100% {
-    opacity: 1;
-  }
-}
-`;
-
-const BTRTitleTracking = styled.div`
-  display: flex;
-  align-self: center;
-  margin: 0;
-
-  color: #90e6c9;
-  text-align: center;
-  font-family: "Pretendard Variable";
-  font-size: 5.25rem;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 125%; /* 6.5625rem */
-  letter-spacing: -0.105rem;
-
-  animation: ${tracking}  0.7s cubic-bezier(0.215, 0.61, 0.355, 1) both;
-}
 `;
 
 const BTRDetail = styled.p`
@@ -346,7 +145,7 @@ const HomeMain = ({ downClick }) => {
 
   const text = "Break The Rules!"; // 타이핑할 문구
   const [displayedText, setDisplayedText] = useState(""); // 상태로 표시할 텍스트 관리
-  const speed = 200; // 글자 타이핑 속도 (밀리초 단위)
+  const speed = 80; // 글자 타이핑 속도 (밀리초 단위)
 
   useEffect(() => {
     let index = 0;
@@ -371,9 +170,6 @@ const HomeMain = ({ downClick }) => {
     <BTRContainer>
       <BTRWrapper>
         <BTRTitleTyping>{displayedText}</BTRTitleTyping>
-        <BTRTitleReveal>Break The Rules!</BTRTitleReveal>
-        <BTRTitleFlick>Break The Rules!</BTRTitleFlick>
-        <BTRTitleTracking>Break The Rules!</BTRTitleTracking>
         <BTRDetail>
           도전과 성장, 열정이 모이는 곳<br />
           당신의 아이디어를 오직 UMC에서
